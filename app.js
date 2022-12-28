260b120169d7dbbbfcf029c3e0bf32a41d6fd0ed
Thiago <thiagodelgado.dev@gmail.com>
2022-12-28 12:33:23 -0300

    responsive design and other details done!

--- app.js
+++ app.js
@@ -6,8 +6,4 @@ const port = 3030;
 app.use(express.static(path.join(__dirname,  'public' )))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
-
-
-
-
 app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port))
